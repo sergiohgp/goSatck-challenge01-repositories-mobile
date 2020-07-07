0854878b6d043528c9a17461b84f4995d0e1a5d2
Sergio Pereira <sergiohgp@gmail.com>
2020-07-07 01:58:19 -0400

    Mobile application

--- src/App.js
+++ src/App.js
@@ -1,55 +1,126 @@
 import React from "react";
+import api from './services/api'
 
 import {
   SafeAreaView,
   View,
+  ScrollView,
   FlatList,
   Text,
   StatusBar,
   StyleSheet,
   TouchableOpacity,
 } from "react-native";
+import { useEffect, useState } from 'react';
 
 export default function App() {
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data)
+    })
+  }, [])
+
+  async function handleAddRepository() {
+    const response = await api.post('repositories', {
+      title: 'New Repository',
+      url: 'new url',
+      techs: ["new tech", "..."]
+    })
+
+    const repository = response.data
+
+    setRepositories([...repositories, repository])
+  }
+
+  async function handleRemoveRepository(id) {
+    await api.delete(`repositories/${id}`)
+
+    const newRepositories = repositories.filter(
+      repository => repository.id !== id
+    )
+    
+    setRepositories(newRepositories)
+  }
+
   async function handleLikeRepository(id) {
     // Implement "Like Repository" functionality
+    const response = await api.post(`repositories/${id}/like`)
+
+    const liked = response.data
+
+    const newRepositories = repositories.map(repository => {
+      if (repository.id === id) {
+        return liked
+      } else {
+        return repository
+      }
+    })
+
+    setRepositories(newRepositories) 
   }
 
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
       <SafeAreaView style={styles.container}>
-        <View style={styles.repositoryContainer}>
-          <Text style={styles.repository}>Repository 1</Text>
+      <View style={styles.addDeleteView}>
+        <TouchableOpacity
+          style={styles.addButton}
+          onPress={() => handleAddRepository()}
+        >
+          <Text style={styles.buttonText}>
+            Adicionar Repositorio Estatico
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <FlatList 
+        style={styles.container}
+        data={repositories}
+        keyExtractor={repository => repository.id}
+        renderItem={({ item: repository}) => (
+          <View style={styles.repositoryContainer}>
+          <Text style={styles.repository}>{repository.title}</Text>
 
-          <View style={styles.techsContainer}>
-            <Text style={styles.tech}>
-              ReactJS
-            </Text>
-            <Text style={styles.tech}>
-              Node.js
-            </Text>
-          </View>
+          
+            <View style={styles.techsContainer}>
+                {repository.techs.map(tech => (
+                  <Text key={tech} style={styles.tech}>
+                    {tech}
+                  </Text>
+                ))}
+            </View>
+          
 
           <View style={styles.likesContainer}>
             <Text
               style={styles.likeText}
               // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
-              testID={`repository-likes-1`}
+              testID={`repository-likes-${repository.id}`}
             >
-              3 curtidas
+              {repository.likes} curtidas
             </Text>
           </View>
 
           <TouchableOpacity
             style={styles.button}
-            onPress={() => handleLikeRepository(1)}
+            onPress={() => handleLikeRepository(repository.id)}
             // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
-            testID={`like-button-1`}
+            testID={`like-button-${repository.id}`}
           >
-            <Text style={styles.buttonText}>Curtir</Text>
+            <Text style={styles.buttonText}>Like</Text>
           </TouchableOpacity>
+          <TouchableOpacity
+          style={styles.button}
+          onPress={() => handleRemoveRepository(repository.id)}
+        >
+          <Text style={styles.buttonText}>Remover</Text>
+        </TouchableOpacity>
         </View>
+        )}  
+      />
+        
       </SafeAreaView>
     </>
   );
@@ -103,5 +174,9 @@ const styles = StyleSheet.create({
     color: "#fff",
     backgroundColor: "#7159c1",
     padding: 15,
+  },
+  addDeleteView: {
+    flex: 0.2,
+    flexDirection: "row",
   },
 });
